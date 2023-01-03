d68a07a3df4b54ab58403882dfc0f1febe94c135
Stig Oskar Ofstad <stigofstad@gmail.com>
2023-01-03 12:05:27 +0100

    docs: recipes and lookups (#15)
    * docs: recipes and lookups
    
    * Update docs/docs/concepts/recipes.md
    
    Co-authored-by: Eirik Ola Aksnes <eoaksnes@gmail.com>
    
    Co-authored-by: Eirik Ola Aksnes <eoaksnes@gmail.com>

--- docs/docs/concepts/recipes.md
+++ docs/docs/concepts/recipes.md
@@ -3,16 +3,165 @@ title: Recipes
 sidebar_position: 2
 ---
 
-To express how a model is stored and presented, we use a concept of recipes. Every blueprint can have two kind of recipes in the Data Modelling Tool.
+To express how a model is _stored_ and how it is _presented_, we use a concept of StorageRecipes and UiRecipes.
 
-A recipe is just a specific instance of a recipe blueprint, and Libraries, plugins and services can use these recipes to make decisions and therefore become easy to replace.
+A recipe is an instance of the core Storage-/UiRecipe blueprints.
 
-## Storage Recipe
+## StorageRecipe
 
-A recipe for how to store the data for a blueprint.
+A recipe for how to store the data for an entity based on the given blueprint type.
 
-Contained means the value is stored in-place, and all primitives are stored with default contained property. However, if a blueprint is referring to another blueprint, then that blueprint can be stored as own independent entity.
+StorageRecipes are mainly to be consumed by the Data Modelling Storage Service.  
 
-## UI Recipe
+For now, StorageRecipes are mainly used to split large models into smaller parts. Either because it makes sense logically, or for performance reasons.
+
+A StorageRecipes may look something like this;
+
+```json
+{
+  "type": "dmss://system/SIMOS/StorageRecipe",
+  "name": "DEFAULT",
+  "description": "",
+  "attributes": [
+    {
+      "name": "content",
+      "type": "object",
+      "contained": false
+    }
+  ]
+}
+```
+
+This StorageRecipes is used for the core `dmss://system/SIMOS/Package`-type. Here we can see that any objects that are a 
+part of the "content" attribute for a package, is not contained in that package entity but is only referred to. Which makes sense logically.  Instead of the actual object, a reference object is added, and it includes the name, type, and _id of the object referenced. 
+
+## UiRecipe
 
 A recipe for how to present data.
+
+UiRecipes follow many of the same patterns as StorageRecipes, but are used when any client is to present an entity to a user. Like in a web application. 
+
+A UiRecipe may look something like this;
+```json
+{
+  "type": "dmss://system/SIMOS/UiRecipe",
+  "name": "Edit",
+  "description": "Default blueprint edit",
+  "plugin": "edit-blueprint",
+  "category": "edit",
+  "config": {
+    "type": "dmss://system/SIMOS/PluginsConfig/EditBlueprint",
+    "fontColor": "blue"
+  }
+}
+```
+This example is a UiRecipe used for entities of the core `dmss://system/SIMOS/Blueprint`-type.  
+This particular recipe is used to load a javascript UI-plugin named "edit-blueprint".
+The "config" value of a UiRecipe is of type "object", and can be any valid blueprint. The object itself is passed to the plugin, and it's up to the plugin to interpret the config object.
+
+
+## RecipeLinks, RecipeLookups and ApplicationContexts
+
+Since different applications may have different needs for viewing the same model, UiRecipes can only be associated with a 
+specific blueprint by creating an __application recipe lookup__.
+
+```mermaid
+flowchart TB;
+    subgraph Lookup 2
+    A2[Application 2]
+    end
+    
+    subgraph Lookup 1
+    A1[Application 1]-->B1[[Blueprint reference]];
+    A1-->B2[[Blueprint reference]];
+    
+    B1--> R1>Recipe]
+    B1--> R2>Recipe]
+    end
+```
+
+This diagram shows the relationship between a named _lookup_, a list of blueprint references, and recipes for each blueprint.
+The lookup object itself "lives" in an DMSS instance, and can be created by either basic API-calls, or using the [DM-CLI](https://github.com/equinor/dm-cli) tool.
+
+Input for the "create application lookup" is a _name_ (the application name should be used), and a path to a (sub)package
+that has been uploaded to DMSS.
+
+```bash
+dm create-lookup myApp MyDataSource/instances/recipe_links
+```
+
+This will create a lookup with name `myApp` in DMSS, based on all the entities of `dmss://system/SIMOS/RecipeLink` found within the `MyDataSource/instances/recipe_links`-package.
+
+For reference, _RecipeLinks_ may look like this;
+
+```json
+{
+  "type": "dmss://system/SIMOS/RecipeLink",
+  "_blueprintPath_": "dmss://system/SIMOS/Blueprint",
+  "uiRecipes": [
+    {
+      "name": "Yaml",
+      "type": "dmss://system/SIMOS/UiRecipe",
+      "plugin": "yaml-view"
+    },
+    {
+      "type": "dmss://system/SIMOS/UiRecipe",
+      "name": "Edit",
+      "description": "Default blueprint edit",
+      "plugin": "edit-blueprint"
+    }
+  ],
+  "storageRecipes": [
+        {
+      "type": "dmss://system/SIMOS/StorageRecipe",
+      "name": "DefaultStorageRecipe",
+      "attributes": [
+        {
+          "name": "attributes",
+          "type": "dmss://system/SIMOS/StorageAttribute",
+          "contained": true
+        }
+      ]
+    }
+  ]
+}
+```
+
+### Web apps and ApplicationContexts
+
+This could be considered an advanced topic, which you might not need to worry about. But understanding the underlying functionality could be useful if you want to do something special.
+
+The main component recommend to render entites in a ReactJS web application is the `<UIPluginSelector>` from the core javascript library. 
+You would use that like so;
+
+```jsx
+import { UIPluginSelector} from '@development-framework/dm-core'
+...
+<UIPluginSelector
+    absoluteDottedId={`${dataSourceId}/${entity._id}`}
+    type={entity.type}
+/>
+...
+```
+
+Now, without any reference to an _application recipe lookup_, this would not find any UiRecipes, and only use the default ones.
+The missing part is wrapping the `<UIPluginSelector>` in an _ApplicationContext_. This is already handled for you in the `create-dm-app`,
+but if you want to do it yourself, it could look something like this;
+
+```jsx
+function App() {
+  const [application, isLoading, updateApplication, error] = useDocument(myApplicationEntityId)
+
+  return (
+    <ApplicationContext.Provider value={application}>
+      <UIPluginSelector
+        absoluteDottedId={`${dataSourceId}/${application?._id}`}
+        type={application?.type}
+      />
+    </ApplicationContext.Provider>
+  )
+}
+```
+
+You can read more about React contexts [here](https://beta.reactjs.org/reference/react/useContext).  
+You only need to wrap you application once, not on every usage of the `<UIPluginSelector>`.
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -2,198 +2,209 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const webpack = require('webpack')
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
-const fs = require('fs');
-const path = require("path");
-const clientDir = path.join(__dirname, '..', 'client');
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
+const fs = require('fs')
+const path = require('path')
+const clientDir = path.join(__dirname, '..', 'client')
 
-const LibrariesToTranspile = [
-    'copy-text-to-clipboard',
-];
+const LibrariesToTranspile = ['copy-text-to-clipboard']
 
 const LibrariesToTranspileRegex = new RegExp(
-    LibrariesToTranspile.map((libName) => `(node_modules/${libName})`).join('|'),
-);
+  LibrariesToTranspile.map((libName) => `(node_modules/${libName})`).join('|')
+)
 
 function excludeJS(modulePath) {
-    // Always transpile client dir
-    if (modulePath.startsWith(clientDir)) {
-        return false;
-    }
-    // Don't transpile node_modules except any docusaurus npm package
-    return (
-        modulePath.includes('node_modules') &&
-        !/docusaurus(?:(?!node_modules).)*\.jsx?$/.test(modulePath) &&
-        !LibrariesToTranspileRegex.test(modulePath)
-    );
+  // Always transpile client dir
+  if (modulePath.startsWith(clientDir)) {
+    return false
+  }
+  // Don't transpile node_modules except any docusaurus npm package
+  return (
+    modulePath.includes('node_modules') &&
+    !/docusaurus(?:(?!node_modules).)*\.jsx?$/.test(modulePath) &&
+    !LibrariesToTranspileRegex.test(modulePath)
+  )
 }
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Development Framework Docs',
-    tagline: 'The framework concists of a collection of libraries, tools and services that can be used to accelerate the building process and maintaince of applications and domain models based on blueprints.',
-    url: 'https://data-modelling-tool.app.radix.equinor.com/',
-    baseUrl: '/dm-docs/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    // favicon: 'img/favicon.ico',
+  title: 'Development Framework Docs',
+  tagline:
+    'The framework concists of a collection of libraries, tools and services that can be used to accelerate the building process and maintaince of applications and domain models based on blueprints.',
+  url: 'https://data-modelling-tool.app.radix.equinor.com/',
+  baseUrl: '/dm-docs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn', // favicon: 'img/favicon.ico',
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'equinor', // Usually your GitHub org/user name.
-    projectName: 'dm-docs', // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'equinor', // Usually your GitHub org/user name.
+  projectName: 'dm-docs', // Usually your repo name.
 
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  themes: ['@docusaurus/theme-live-codeblock', '@docusaurus/theme-mermaid'],
+  markdown: {
+    mermaid: true,
+  },
 
-    themes: ['@docusaurus/theme-live-codeblock'],
-
-    plugins: [
-        [
-            '@docusaurus/plugin-content-docs',
-            {
-                id: 'community',
-                path: 'community',
-                routeBasePath: '/',
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'community',
+        path: 'community',
+        routeBasePath: '/',
+      },
+    ],
+    function (context, options) {
+      return {
+        name: 'webpack-configuration-plugin',
+        configureWebpack(config, isServer, utils) {
+          config.plugins = config.plugins.filter(
+            (plugin) => !(plugin instanceof ModuleScopePlugin)
+          )
+          let rule = config.module.rules[5]
+          // @ts-ignore
+          rule.exclude = excludeJS
+          config.resolve.roots = [
+            ...config.resolve.roots,
+            path.resolve(__dirname, '../web/packages/dmt-core'),
+          ]
+          // @ts-ignore
+          rule.include = [
+            fs.realpathSync(path.join(__dirname)),
+            fs.realpathSync(
+              path.join(
+                __dirname,
+                'node_modules/@development-framework/dm-core/src'
+              )
+            ),
+          ]
+          config.resolve.alias['react'] = path.resolve(
+            __dirname,
+            './node_modules/react'
+          )
+          config.resolve.alias['react-dom'] = path.resolve(
+            __dirname,
+            './node_modules/react-dom'
+          )
+          config.plugins.push(
+            new webpack.ProvidePlugin({
+              process: 'process/browser',
+            })
+          )
+          return {
+            resolve: {
+              fallback: {
+                path: require.resolve('path-browserify'),
+                fs: false,
+              },
+              symlinks: true,
             },
-        ],
-        function (context, options) {
-            return {
-                name: 'webpack-configuration-plugin',
-                configureWebpack(config, isServer, utils) {
-                    config.plugins = config.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
-                    let rule = config.module.rules[5]
-                    // @ts-ignore
-                    rule.exclude = excludeJS
-                    config.resolve.roots = [
-                        ...config.resolve.roots,
-                        path.resolve(__dirname, '../web/packages/dmt-core')
-                    ]
-                    // @ts-ignore
-                    rule.include = [
-                        fs.realpathSync(path.join(__dirname)),
-                        fs.realpathSync(path.join(__dirname, 'node_modules/@development-framework/dm-core/src')),
-                    ]
-                    config.resolve.alias["react"] = path.resolve(__dirname, './node_modules/react')
-                    config.resolve.alias["react-dom"] = path.resolve(__dirname, './node_modules/react-dom')
-                    config.plugins.push(
-                        new webpack.ProvidePlugin({
-                            process: 'process/browser'
-                        })
-                    )
-                    return {
-                        resolve: {
-                            fallback: {
-                                "path": require.resolve("path-browserify"),
-                                fs: false
-                            },
-                            symlinks: true
-                        },
-                    }
-                }
-            };
+          }
         },
-    ],
+      }
+    },
+  ],
 
-    presets: [
-        [
-            'classic',
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    // editUrl:
-                    //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
-                blog: false,
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-            }),
-        ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'), // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
     ],
+  ],
 
-    themeConfig:
+  themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            navbar: {
-                title: 'Development Framework',
-                //logo: {
-                //    alt: 'My Site Logo',
-                //    src: 'img/logo.svg',
-                //},
-                items: [
-                    {
-                        type: 'doc',
-                        docId: 'introduction',
-                        position: 'left',
-                        label: 'Docs',
-                    },
-                    {
-                        type: 'doc',
-                        docId: 'community',
-                        docsPluginId: 'community',
-                        position: 'left',
-                        label: 'Community',
-                    },
-                    {
-                        href: 'https://github.com/equinor/dm-docs',
-                        label: 'GitHub',
-                        position: 'right',
-                    },
-                ],
-            },
-            footer: {
-                style: 'dark',
-                links: [
-                    {
-                        title: 'Docs',
-                        items: [
-                            {
-                                label: 'Getting started',
-                                to: '/docs/getting-started',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'Community',
-                        items: [
-                            {
-                                label: 'GitHub Issues',
-                                href: 'https://github.com/equinor/dm-docs/issues',
-                            }
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
-                                label: 'Community',
-                                to: '/community',
-                            },
-                            {
-                                label: 'GitHub',
-                                href: 'https://github.com/equinor/dm-docs',
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()} Equinor`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-        }),
-};
+    ({
+      navbar: {
+        title: 'Development Framework', //logo: {
+        //    alt: 'My Site Logo',
+        //    src: 'img/logo.svg',
+        //},
+        items: [
+          {
+            type: 'doc',
+            docId: 'introduction',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            type: 'doc',
+            docId: 'community',
+            docsPluginId: 'community',
+            position: 'left',
+            label: 'Community',
+          },
+          {
+            href: 'https://github.com/equinor/dm-docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting started',
+                to: '/docs/getting-started',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'GitHub Issues',
+                href: 'https://github.com/equinor/dm-docs/issues',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Community',
+                to: '/community',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/equinor/dm-docs',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Equinor`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+}
 
-module.exports = config;
+module.exports = config
